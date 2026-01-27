4a5f41dc1b3adafb0aa01fd5c08756bdaec9599d
CarlaFdezdh <carlafdezdh@gmail.com>
2026-01-27 10:31:10 +0100

    temas colores

--- Android/app/src/main/java/com/example/autolog/ui/theme/Color.kt
+++ Android/app/src/main/java/com/example/autolog/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.autolog.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFCAF0F8)
+val PurpleGrey80 = Color(0xFF0077B6)
+val Pink80 = Color(0xFFF2FDFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF687B80)
+val PurpleGrey40 = Color(0xFF013C5B)
+val Pink40 = Color(0xFF878F8F)
